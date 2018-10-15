02c7b9aeac85ea99240ee2c22323bcb9f03ba8ac
Kuyul <peyopeyo92@gmail.com>
2018-10-15 13:45:39 +1300

    ff

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void GenerateLevel()
+    {
+
+    }
+}
