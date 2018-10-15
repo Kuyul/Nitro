c9c3b62c06efd9b550785f328f6d08e5f06a852c
Kuyul <peyopeyo92@gmail.com>
2018-10-15 14:00:20 +1300

    Level Generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	public void GenerateLevel()
-    {
+    //Declare public variables
+    public int NumberOfPlatforms = 30;
+    public Transform InitialPos;
+    public GameObject[] Platforms;
+    public float PlatformOffset = 2.0f;
 
+    private void Start()
+    {
+        GenerateLevel();
+    }
+
+    public void GenerateLevel()
+    {
+        for(int i = 0; i < NumberOfPlatforms; i++)
+        {
+            Vector3 newPos = InitialPos.position + new Vector3(0, i * PlatformOffset, 0);
+            Instantiate(Platforms[Random.Range(0, Platforms.Length)], newPos, Quaternion.identity);
+        }
     }
 }
