09191599e99da7cc20bf3c07ec44326961ed5da5
Kuyul <peyopeyo92@gmail.com>
2018-10-17 16:49:26 +1300

    Moving Patterns

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -6,15 +6,19 @@ public class FollowPath : MonoBehaviour
 {
 
     // The target marker.
-    public Transform LeaderObj;
     public Transform[] FollowerObjs;
     public Transform[] Targets;
     public float speed;
     public PathTypes PathType;
+    public bool Triggered = false;
+    //Can also spin
+    public float AngularVel = 0;
 
+    //Declare private variables
     private float step;
     private int Pathindex;
     private List<List<Vector3>> ListOfStoredPositions;
+
 
     #region Enums
     public enum PathTypes //Types of movement paths
@@ -27,56 +31,75 @@ public class FollowPath : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        step = Time.deltaTime * speed;
-        Pathindex = 0;
-        //Populate path storage
-        ListOfStoredPositions = new List<List<Vector3>>();
-        for (int i = 0; i < FollowerObjs.Length; i++)
+        if (Targets.Length > 0)
         {
-            ListOfStoredPositions.Add(new List<Vector3>());
+            Pathindex = 0;
+            //Populate path storage
+            ListOfStoredPositions = new List<List<Vector3>>();
+            for (int i = 0; i < FollowerObjs.Length; i++)
+            {
+                ListOfStoredPositions.Add(new List<Vector3>());
+            }
+            //Set the initial position of the moving gameobject to the first target
+            transform.localPosition = Targets[0].localPosition;
+        }
+        //set angular vel
+        if (AngularVel != 0)
+        {
+            GetComponent<Rigidbody2D>().angularVelocity = AngularVel;
         }
     }
 
     void Update()
     {
-        //if the number of follower spheres have not been set, return
-
-        //Follow the leader sphere
-        ListOfStoredPositions[0].Add(LeaderObj.localPosition);
-        LeaderObj.localPosition = Vector3.MoveTowards(LeaderObj.localPosition, Targets[Pathindex].localPosition, step);
-
-        //Loop through each follower spheres and add its position to the List of Stored Positions
-        for (int i = 0; i < FollowerObjs.Length; i++)
+        if (Triggered)
         {
-            //This if statement sets the distance between the spheres
-            if (ListOfStoredPositions[i].Count > 20 / speed)
+            //Follow the leader
+            if (ListOfStoredPositions.Count > 0)
             {
-                //We don't need to include the tail sphere to the list
-                if (i != FollowerObjs.Length - 1)
+                ListOfStoredPositions[0].Add(transform.localPosition);
+            }
+            step = Time.deltaTime * speed;
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Targets[Pathindex].localPosition, step);
+
+            //Loop through each follower spheres and add its position to the List of Stored Positions
+            for (int i = 0; i < FollowerObjs.Length; i++)
+            {
+                //This if statement sets the distance between the spheres
+                if (ListOfStoredPositions[i].Count > 20 / speed)
                 {
-                    //Add the position of the current follower sphere for the one behind to trace
-                    ListOfStoredPositions[i + 1].Add(FollowerObjs[i].localPosition);
+                    //We don't need to include the tail sphere to the list
+                    if (i != FollowerObjs.Length - 1)
+                    {
+                        //Add the position of the current follower sphere for the one behind to trace
+                        ListOfStoredPositions[i + 1].Add(FollowerObjs[i].localPosition);
+                    }
+                    FollowerObjs[i].localPosition = ListOfStoredPositions[i][0];
+                    ListOfStoredPositions[i].RemoveAt(0);
                 }
-                FollowerObjs[i].localPosition = ListOfStoredPositions[i][0];
-                ListOfStoredPositions[i].RemoveAt(0);
             }
-        }
 
-        if (LeaderObj.localPosition == Targets[Pathindex].localPosition)
-        {
-            Pathindex++;
-        }
+            if (transform.localPosition == Targets[Pathindex].localPosition)
+            {
+                Pathindex++;
+            }
 
-        if (Pathindex >= Targets.Length)
-        {
-            if (PathType == PathTypes.loop)
+            if (Pathindex >= Targets.Length)
             {
-                Pathindex = 0;
-            }
-            else
-            {
-                Pathindex--;//Dummy
+                if (PathType == PathTypes.loop)
+                {
+                    Pathindex = 0;
+                }
+                else
+                {
+                    Pathindex--;//Dummy
+                }
             }
         }
+    }
+
+    public void SetTrigger()
+    {
+        Triggered = true;
     }
 }
